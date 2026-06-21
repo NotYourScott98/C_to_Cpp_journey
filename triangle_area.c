32dc9f6ea84825b71def7c6bcd78a5c89ce22345
NotYourScott98 <gsaswati35@gmail.com>
2026-06-21 18:17:20 +0530

    Create triangle_area.c

---
+++ triangle_area.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c, sp, area;
+    printf("enter sides of the triangle: ");
+    scanf("%f,%f,%f", &a, &b, &c);
+
+    sp = (a + b + c) / 2;
+    area = (sqrt(sp * (sp - a) * (sp - b) * (sp - c)));
+
+    printf("area of the triangle is: %f\n", area);
+    printf("perimeter of the triangle is: %f", 2 * sp);
+
+    return 0;
+}
